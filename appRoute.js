e638c9862387a2f4f126bd1128c0e310cd6adea9
angelatto <pudri109@gmail.com>
2021-04-21 18:30:51 +0900

    [update] logo and menu tab bar

--- appRoute.js
+++ appRoute.js
@@ -4,7 +4,6 @@ angular.module("app")
     $routeProvider
     // 왼쪽은 라우트 경로, templateUrl은 물리적인 경로
     .when("/", {templateUrl: "views/palbang_m/palbang_m_index.html"})
-   
     .when("/palbang_m", {templateUrl:"views/palbang_m/palbang_m_index.html", controller:"palbangController"})
    
    
