ff5c7bd4bb6948bdb2e9db7f0baf1a186c17d3c1
angelatto <pudri109@gmail.com>
2021-04-24 17:50:43 +0900

    [add] angular chart-bar chart-doughnut template test-complete

--- controllers/inquiryController.js
+++ controllers/inquiryController.js
@@ -100,14 +100,19 @@ angular.module("app")
         };
 
         // test
-        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-        $scope.series = ['Series A', 'Series B'];
-        $scope.data = [
-          [65, 59, 80, 81, 56, 55, 40],
-          [28, 48, 40, 19, 86, 27, 90]
-        ];
-        $scope.onClick = function (points, evt) {
-          console.log(points, evt);
-        };
+       // 막대 그래프 테스트 
+       $scope.labels1 = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+       $scope.series1 = ['Series A', 'Series B', 'SeriesC'];
+     
+       $scope.data1 = [
+         [65, 59, 80, 81, 56, 55, 40],
+         [28, 48, 40, 19, 86, 27, 90],
+         [28, 48, 40, 19, 86, 27, 90]
+       ];
+
+       // 도넛 그래프 테스트 
+       $scope.labels2 = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+       $scope.data2 = [300, 500, 100];
+
        
     });
