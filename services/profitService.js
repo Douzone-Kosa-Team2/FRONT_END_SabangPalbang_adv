cc03e07242daede5b24e6d767ad425ec41a65cd4
JongHyun94 <ant9406@naver.com>
2021-04-24 16:42:24 +0900

    [update] 컨트롤러+서비스

--- services/profitService.js
+++ services/profitService.js
@@ -2,6 +2,12 @@ angular.module("app")
     .factory("profitService", function($http){
 
         const BASE_URL = "http://localhost:8080/profit_m";
+        //채정 - 멤버
+        const ADD_URL_MEMBER = "/member";
+        //종현 - 사방
+        const ADD_URL_SABANG = "/sabang";
+        //민상 - 주문
+        const ADD_URL_ORDER = "/order";
         return {
 
         }
