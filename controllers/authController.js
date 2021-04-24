e14dcec5824d064f968e15d480773a7d58f12b9b
angelatto <pudri109@gmail.com>
2021-04-24 22:59:47 +0900

    [update] login 성공 시 실적관리 페이지로 이동.

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,10 +2,8 @@ angular.module("app")
     .controller("authController", function($scope, authService, $window, $rootScope, $location) {
         
         $scope.login = (user) => {
-            console.log("1");
             authService.login(user)
             .then((response) => {
-                console.log("2");
                 $rootScope.uid = response.data.uid;
                 $rootScope.authToken = response.data.authToken;
 
@@ -13,12 +11,10 @@ angular.module("app")
                 sessionStorage.setItem("authToken",response.data.authToken);
                 //console.log($rootScope.uid);
                 //console.log($rootScope.authToken);
-                $location.url("/sabang_m");
+                $location.url("/profit_m");
             })
             .catch ((response) => {
-                console.log("3");
                 $window.alert("로그인 실패:" + response.data.message);
             });
         };
-
     });
